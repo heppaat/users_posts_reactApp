1bc53cb5fdd928773223725c2dd6773aa5616fe2
heppaat <pali.herrer@gmail.com>
2024-08-15 11:39:03 +0200

    filteredPosts render works

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { fetchData, Post } from "./lib/api";
 function App() {
   const [posts, setPosts] = useState<Post[]>([]);
   const [error, setError] = useState<string | null>(null);
+  const [selectedUser, setSelectedUser] = useState<number | null>(null);
 
   useEffect(() => {
     const getPosts = async () => {
@@ -26,12 +27,19 @@ function App() {
     return acc;
   }, []);
 
+  const handleSelectUser = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const userId = parseInt(event.target.value, 10);
+    setSelectedUser(userId);
+  };
+
+  const filteredPosts = posts.filter((post) => post.userId === selectedUser);
+
+  if (error) return <div>Error: {error}</div>;
+
   return (
     <div className="select-container">
-      <select name="users" id="select-users">
-        <option value="" disabled>
-          Users
-        </option>
+      <select name="users" id="select-users" onChange={handleSelectUser}>
+        <option value="">Users</option>
         {uniqueUserIds.map((userId) => (
           <option key={userId} value={userId}>
             User: {userId}
@@ -40,6 +48,11 @@ function App() {
       </select>
       <select name="posts" id="select-posts">
         <option value="">Posts</option>
+        {filteredPosts.map((post) => (
+          <option key={post.id} value={post.id}>
+            {post.title}
+          </option>
+        ))}
       </select>
     </div>
   );
